ab1886d6f15a304e135e9c9831840dddbb396970
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2015-03-11 00:16:08 +0100

    Made the gallery a bit nicer.
    Added Angular filter that can be used to split an array into chunks. This is used in the gallery to only allow a certain number of images in a single row div.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,35 @@
 
 (function() {
 
-	var app = angular.module("campi", ['ui.select', 'ngSanitize', 'ui.bootstrap']);
+	var app = angular.module("campi", ['ui.select', 'ngSanitize', 'ui.bootstrap', 'angular-underscore']);
+
+	app.filter('groupBy', function() {
+
+		return _.memoize(function(input, chsize) {
+			if (input == undefined || chsize == undefined || typeof chsize !== 'number' || chsize % 1 !== 0 || chsize <= 0) {
+				return;
+			}
+
+			var out = [];
+
+			for (var i = 0; i < input.length; i += chsize) {
+				out.push(input.slice(i, i + chsize));
+			}
+
+			return out;
+		});
+	});
 
 
 	app.controller("camera_controller", ['$scope', '$http', function($scope, $http) {
 
 		$scope.options = {
-				'mode': {'type': 'multi',
-					values: [{name: 'photo', pretty: 'Photo'},
-					         {name: 'timelapse', pretty: 'Timelapse'},
-					         {name: 'video', pretty: 'Video'}],
-					         'default': 0},
-					 
+				'mode': {type: 'multi',
+				         values: [{name: 'photo', pretty: 'Photo'},
+				                  {name: 'timelapse', pretty: 'Timelapse'},
+				                  {name: 'video', pretty: 'Video'}],
+				         default: 0},
+
 				'width': {type: 'int', min: 0, max: 2500, default: 1024},
 				'height': {type: 'int', min: 0, max: 2000, default: 768},
 				'quality': {type: 'slider', min: 0, max: 100, default: 85},
@@ -24,60 +41,60 @@
 				'saturation': {type: 'slider', min: -100, max: 100, default: 0},
 				'ev': {type: 'slider', min: -2, max: 2, default: 0},
 				'ex': {type: 'multi', 
-					values: [{name: 'auto', pretty: 'Auto'}, 
-					         {name: 'off', pretty: 'Off'}, 
-					         {name: 'night', pretty: 'Night'}, 
-					         {name: 'nightpreview', pretty: 'Night preview'},
-					         {name: 'backlight', pretty: 'Backlight'},
-					         {name: 'spotlight', pretty: 'Spotlight'},
-					         {name: 'sports', pretty: 'Sports'},
-					         {name: 'snow', pretty: 'Snow'}, 
-					         {name: 'beach', pretty: 'Beach'},
-					         {name: 'verylong', pretty: 'Very long'},
-					         {name: 'fixedfps', pretty: 'Fixed FPS'},
-					         {name: 'antishake', pretty: 'Anti-shake'},
-					         {name: 'fireworks', pretty: 'Fireworks'}], 
-				default: 0},
+				       values: [{name: 'auto', pretty: 'Auto'}, 
+				                {name: 'off', pretty: 'Off'}, 
+				                {name: 'night', pretty: 'Night'}, 
+				                {name: 'nightpreview', pretty: 'Night preview'},
+				                {name: 'backlight', pretty: 'Backlight'},
+				                {name: 'spotlight', pretty: 'Spotlight'},
+				                {name: 'sports', pretty: 'Sports'},
+				                {name: 'snow', pretty: 'Snow'}, 
+				                {name: 'beach', pretty: 'Beach'},
+				                {name: 'verylong', pretty: 'Very long'},
+				                {name: 'fixedfps', pretty: 'Fixed FPS'},
+				                {name: 'antishake', pretty: 'Anti-shake'},
+				                {name: 'fireworks', pretty: 'Fireworks'}], 
+				       default: 0},
 				'awb': {type: 'multi',
-					values: [{name: 'auto', pretty: 'Auto'}, 
-					         {name: 'off', pretty: 'Off'},
-					         {name: 'sun', pretty: 'Sun'},
-					         {name: 'cloud', pretty: 'Cloud'},
-					         {name: 'shade', pretty: 'Shade'},
-					         {name: 'tungsten', pretty: 'Tungsten'},
-					         {name: 'fluorescent', pretty: 'Fluorescent'},
-					         {name: 'incandescent', pretty: 'Incandescent'},
-					         {name: 'flash', pretty: 'Flash'},
-					         {name: 'horizon', pretty: 'Horizon'}],
-				default: 0},
+				        values: [{name: 'auto', pretty: 'Auto'}, 
+				                 {name: 'off', pretty: 'Off'},
+				                 {name: 'sun', pretty: 'Sun'},
+				                 {name: 'cloud', pretty: 'Cloud'},
+				                 {name: 'shade', pretty: 'Shade'},
+				                 {name: 'tungsten', pretty: 'Tungsten'},
+				                 {name: 'fluorescent', pretty: 'Fluorescent'},
+				                 {name: 'incandescent', pretty: 'Incandescent'},
+				                 {name: 'flash', pretty: 'Flash'},
+				                 {name: 'horizon', pretty: 'Horizon'}],
+					    default: 0},
 				'ifx': {type: 'multi',
-					values: [{name: 'none', pretty: 'None'},
-					         {name: 'negative', pretty: 'Negative'},
-					         {name: 'solarise', pretty: 'Solarise'},
-					         {name: 'sketch', pretty: 'Sketch'},
-					         {name: 'denoise', pretty: 'Denoise'},
-					         {name: 'emboss', pretty: 'Emboss'},
-					         {name: 'oilpaint', pretty: 'Oil painting'},
-					         {name: 'hatch', pretty: 'Hatch'},
-					         {name: 'gpen', pretty: 'GPen'},
-					         {name: 'pastel', pretty: 'Pastel'},
-					         {name: 'watercolour', pretty: 'Watercolour'},
-					         {name: 'film', pretty: 'Film'},
-					         {name: 'blur', pretty: 'Blur'},
-					         {name: 'saturation', pretty: 'Saturation'},
-					         {name: 'colourswap', pretty: 'Colour swap'},
-					         {name: 'washedout', pretty: 'Washed out'},
-					         {name: 'posterise', pretty: 'Posterise'}, 
-					         {name: 'colourpoint', pretty: 'Colour point'},
-					         {name: 'colourbalance', pretty: 'Colour balance'},
-					         {name: 'cartoon', pretty: 'Cartoon'}],
-				default: 0},
+				        values: [{name: 'none', pretty: 'None'},
+				                 {name: 'negative', pretty: 'Negative'},
+				                 {name: 'solarise', pretty: 'Solarise'},
+				                 {name: 'sketch', pretty: 'Sketch'},
+				                 {name: 'denoise', pretty: 'Denoise'},
+				                 {name: 'emboss', pretty: 'Emboss'},
+				                 {name: 'oilpaint', pretty: 'Oil painting'},
+				                 {name: 'hatch', pretty: 'Hatch'},
+				                 {name: 'gpen', pretty: 'GPen'},
+				                 {name: 'pastel', pretty: 'Pastel'},
+				                 {name: 'watercolour', pretty: 'Watercolour'},
+				                 {name: 'film', pretty: 'Film'},
+				                 {name: 'blur', pretty: 'Blur'},
+				                 {name: 'saturation', pretty: 'Saturation'},
+				                 {name: 'colourswap', pretty: 'Colour swap'},
+				                 {name: 'washedout', pretty: 'Washed out'},
+				                 {name: 'posterise', pretty: 'Posterise'}, 
+				                 {name: 'colourpoint', pretty: 'Colour point'},
+				                 {name: 'colourbalance', pretty: 'Colour balance'},
+				                 {name: 'cartoon', pretty: 'Cartoon'}],
+				        default: 0},
 				'metering': {type: 'multi',
-					values: [{name: 'average', pretty: 'Average'},
-					         {name: 'spot', pretty: 'Spot'},
-					         {name: 'backlit', pretty: 'Backlit'},
-					         {name: 'matrix', pretty: 'Matrix'}],
-					         default: 0},
+				             values: [{name: 'average', pretty: 'Average'},
+				                      {name: 'spot', pretty: 'Spot'},
+				                      {name: 'backlit', pretty: 'Backlit'},
+				                      {name: 'matrix', pretty: 'Matrix'}],
+				             default: 0},
 				'bitrate': {type: 'int', min: 100000, max: 5000000, default: 1000000},
 				'framerate': {type: 'int', min: 1, max: 90, default: 25},
 				'vstab': {type: 'bool', default: false},
@@ -140,5 +157,6 @@
 	app.config(function(uiSelectConfig) {
 		uiSelectConfig.searchEnabled = false;
 	});
+
 
 })();
